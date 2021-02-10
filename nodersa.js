331137931994d04903797d83478484c3ee3a7940
0xrfsd <64338100+0xrfsd@users.noreply.github.com>
2021-02-10 19:49:54 -0300

    V2() => Exporting and logging public key + private key

--- nodersa.js
+++ nodersa.js
@@ -2,7 +2,15 @@ const NodeRSA = require('node-rsa');
 const key = new NodeRSA({b: 512});
  
 const text = 'Hello RSA!';
+
 const encrypted = key.encrypt(text, 'base64');
 console.log('encrypted: ', encrypted);
+
 const decrypted = key.decrypt(encrypted, 'utf8');
 console.log('decrypted: ', decrypted);
+
+var public_key = key.exportKey('public');
+
+var private_key = key.exportKey('public');
+
+console.log(public_key + '\n' + private_key);
